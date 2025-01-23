0557b843d0f018ea6645adec5d5dc0df1215c47f
mdavi89 <mdavi359@yahoo.com>
2025-01-23 17:58:06 -0500

    Update index.ts

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,28 +1,16 @@
 import type { Request, Response } from 'express';
 import express from 'express';
-import { createProxyMiddleware } from 'http-proxy-middleware';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-
 const router = express.Router();
 
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+import apiRoutes from './api/index.js';
 
-// Proxy API requests to server.com
-router.use(
-  '/api',
-  createProxyMiddleware({
-    target: 'https://test-app-ci-cd-server.onrender.com/', // Replace with your server URL
-    changeOrigin: true,           // Ensures the host header matches the target
-    pathRewrite: {
-      '^/api': '/api',            // Optional: Adjust if your server uses a different path structure
-    },
-    secure: true,                 // Use `true` if `server.com` uses HTTPS; set `false` for self-signed certs
-  })
-);
+router.use('/api', apiRoutes);
 
-// Serve up React front-end in production
+// serve up react front-end in production
 router.use((_req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, '../../../client/dist/index.html'));
 });
